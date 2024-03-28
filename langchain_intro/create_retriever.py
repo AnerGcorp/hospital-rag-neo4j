5fa743d5f88a7286b2465b6c1439704307d295d1
Anergcorp <ahunatajanov@gmail.com>
2024-03-28 23:25:44 +0200

    implementing retrieval chain

---
+++ langchain_intro/create_retriever.py
@@ -0,0 +1,23 @@
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain_community.vectorstores.chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+import dotenv
+
+REVIEWS_CSV_PATH = 'data/reviews.csv'
+REVIEWS_CHROMA_PATH = 'chroma_data'
+
+dotenv.load_dotenv()
+
+loader = CSVLoader(
+    file_path=REVIEWS_CSV_PATH,
+    source_column='review'
+)
+
+reviews = loader.load()
+
+reviews_vector_db = Chroma.from_documents(
+    documents=reviews,
+    embedding=OpenAIEmbeddings(),
+    persist_directory=REVIEWS_CHROMA_PATH
+)
+
