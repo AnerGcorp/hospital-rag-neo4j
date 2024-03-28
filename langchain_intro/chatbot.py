3a6b212bc814fc29c85fab13ca81c485595e3226
Anergcorp <ahunatajanov@gmail.com>
2024-03-29 00:22:04 +0200

    implementing retrieval agent

--- langchain_intro/chatbot.py
+++ langchain_intro/chatbot.py
@@ -9,6 +9,13 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_community.vectorstores.chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain.schema.runnable import RunnablePassthrough
+from langchain.agents import (
+    create_openai_functions_agent,
+    Tool,
+    AgentExecutor
+)
+from langchain import hub
+from tools import get_current_wait_time
 import dotenv
 
 dotenv.load_dotenv()
@@ -58,7 +65,53 @@ review_chain = (
     | StrOutputParser()
 )
 
-question = """Has anyone complained about communication with the hospital staff?"""
+# tools
+tools = [
+    Tool(
+        name="Reviews",
+        func=review_chain.invoke,
+        description="""Useful when you need to answer questions
+        about patient reviews or experiences at the hospital.
+        Not useful for answering questions about specific visit
+        details such as payer, billing, treatment, diagnosis,
+        chief complaint, hospital, or physician information.
+        Pass the entire question as input to the tool. For instance,
+        if the question is "What do patients think about the triage system?",
+        the input should be "What do patients think about the triage system?"
+        """
+    ),
+    Tool(
+        name="Waits",
+        func=get_current_wait_time,
+        description="""Use when asked about current wait times
+        at a specific hospital. This tool can only get the current
+        wait time at a hospital and does not have any information about
+        aggregate or historical wait times. This tool returns wait times in
+        minutes. Do not pass the word "hospital" as input,
+        only the hospital name itself. For instance, if the question is
+        "What is the wait time at hospital A?", the input should be "A".
+        """
+    )
+]
 
-print(review_chain.invoke(question))
+hospital_agent_prompt = hub.pull("hwchase17/openai-functions-agent")
 
+agent_chat_model = ChatOpenAI(
+    model="gpt-3.5-turbo",
+    temperature=0
+)
+
+hospital_agent = create_openai_functions_agent(
+    llm=agent_chat_model,
+    prompt=hospital_agent_prompt,
+    tools=tools
+)
+
+hospital_agent_executor = AgentExecutor(
+    agent=hospital_agent,
+    tools=tools,
+    return_intermediate_steps=True,
+    verbose=True
+)
+
+print(hospital_agent_executor.invoke({"input": "What is the current wait time at hospital C?"}))
