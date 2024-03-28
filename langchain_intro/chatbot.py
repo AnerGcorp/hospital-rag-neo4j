c4fd0742abff0d759e92045d0309af629d13ac44
Anergcorp <ahunatajanov@gmail.com>
2024-03-28 21:23:44 +0200

    implementing chatPromoptTemplate from f-strings

--- langchain_intro/chatbot.py
+++ langchain_intro/chatbot.py
@@ -1,4 +1,7 @@
 from langchain_openai import ChatOpenAI
+from langchain.prompts import (
+    ChatPromptTemplate
+)
 from langchain.schema.messages import HumanMessage, SystemMessage
 import dotenv
 
@@ -6,12 +9,29 @@ dotenv.load_dotenv()
 
 chat_model = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
 
+review_template_str = """Your job is to use patient
+    reviews to answer questions about their experience at a hospital.
+    Use the following context to answer questions. Be as detailed
+    as possible, but don't make up any information that's not
+    from the context. If you don't know an answer, say you don't know.
+
+    {context}
+
+    {question}
+"""
+
+review_template = ChatPromptTemplate.from_template(review_template_str)
+
+context = "I had a great stay!"
+question = "Did anyone have a positive experience?"
+
+
 
 messages = [
     SystemMessage(
         content="""You're an assistant knowledgeable about
         healthcare. Only answer healthcare-related questions."""
     ),
-    HumanMessage(content="What is Medicaid managed care?"),
+    HumanMessage(content="How do I change a tire?"),
 ]
 print(chat_model.invoke(messages))
