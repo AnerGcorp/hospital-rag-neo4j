50b47e48479b760528ec7e3dfc05bed12439bd45
Anergcorp <ahunatajanov@gmail.com>
2024-03-28 22:24:20 +0200

    implementing chains

--- langchain_intro/chatbot.py
+++ langchain_intro/chatbot.py
@@ -1,6 +1,10 @@
 from langchain_openai import ChatOpenAI
 from langchain.prompts import (
-    ChatPromptTemplate
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate,
+    PromptTemplate
 )
 from langchain.schema.messages import HumanMessage, SystemMessage
 import dotenv
@@ -10,21 +14,43 @@ dotenv.load_dotenv()
 chat_model = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
 
 review_template_str = """Your job is to use patient
-    reviews to answer questions about their experience at a hospital.
-    Use the following context to answer questions. Be as detailed
-    as possible, but don't make up any information that's not
-    from the context. If you don't know an answer, say you don't know.
+reviews to answer questions about their experience at
+a hospital. Use the following context to answer questions.
+Be as detailed as possible, but don't make up any information
+that's not from the context. If you don't know an answer, say
+you don't know.
 
-    {context}
-
-    {question}
+{context}
 """
 
-review_template = ChatPromptTemplate.from_template(review_template_str)
+
+review_system_prompt = SystemMessagePromptTemplate(
+    prompt=PromptTemplate(
+        input_variables=['context'],
+        template=review_template_str
+    )
+)
+
+review_human_prompt = HumanMessagePromptTemplate(
+    prompt=PromptTemplate(
+        input_variables=['question'],
+        template="{question}"
+    )
+)
+
+messages = [review_system_prompt, review_human_prompt]
+
+review_prompt_template = ChatPromptTemplate(
+    input_variables=["context", "question"],
+    messages=messages
+)
+
+review_chain = review_prompt_template | chat_model
 
 context = "I had a great stay!"
-question = "Did anyone have a positive experience?"
+question = "What is blood pressure?"
 
+review_chain.invoke({"context": context, "question": question})
 
 
 messages = [
