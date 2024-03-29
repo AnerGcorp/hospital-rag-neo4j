1ccdb190a436751068c821d020023e1af6d8022a
Anergcorp <ahunatajanov@gmail.com>
2024-03-29 02:17:31 +0200

    loading to neo4j hospitals data

--- hospital_neo4j_etl/src/hospital_bulk_csv_write.py
+++ hospital_neo4j_etl/src/hospital_bulk_csv_write.py
@@ -22,3 +22,36 @@ logging.basicConfig(
 )
 
 LOGGER = logging.getLogger(__name__)
+
+NODES = ["Hospital", "Payer", "Physician", "Patient", "Visit", "Review"]
+
+def _set_uniqueness_constraints(tx, node):
+    query = f"""CREATE CONSTRAINT IF NOT EXISTS FOR (n:{node})
+        REQUIRE n.id IS UNIQUE;"""
+    _ = tx.run(query, {})
+
+
+@retry(tries=100, delay=10)
+def load_hospital_graph_from_csv() -> None:
+    """Load structured hospital CSV data following
+    a specific ontology into Neo4j"""
+
+    driver = GraphDatabase.driver(
+        NEO4J_URI, auth=(NEO4J_USERNAME, NEO4J_PASSWORD)
+    )
+
+    LOGGER.info("Setting uniqueness constraints on nodes")
+    with driver.session(database="neo4j") as session:
+        for node in NODES:
+            session.execute_write(_set_uniqueness_constraints, node)
+    
+    LOGGER.info("Loading hospital nodes")
+    with driver.session(database="neo4j") as session:
+        query = f"""
+        LOAD CSV WITH HEADERS
+        FROM '{HOSPITALS_CSV_PATH}' AS hospitals
+        MERGE (h:Hospital {{id: toInteger(hospitals.hospital_id),
+                            name: hospitals.hospital_name,
+                            state_name: hospitals.hospital_state}});
+        """
+        _ = session.run(query, {})
