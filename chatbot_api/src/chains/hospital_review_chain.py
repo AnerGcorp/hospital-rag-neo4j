e051fa4b2afeeb0800fd5b33952d846ee2b21f8a
Anergcorp <ahunatajanov@gmail.com>
2024-04-05 03:50:23 +0300

    Initializing vector graph

--- chatbot_api/src/chains/hospital_review_chain.py
+++ chatbot_api/src/chains/hospital_review_chain.py
@@ -9,3 +9,21 @@ from langchain.prompts import (
     HumanMessagePromptTemplate,
     ChatPromptTemplate,
 )
+
+HOSPITAL_QA_MODEL = os.getenv("HOSPITAL_QA_MODEL")
+
+neo4j_vector_index = Neo4jVector.from_existing_graph(
+    embedding=OpenAIEmbeddings(),
+    url=os.getenv("NEO4J_URI"),
+    username=os.getenv("NEO4J_USERNAME"),
+    password=os.getenv("NEO4J_PASSWORD"),
+    index_name="reviews",
+    node_label="Review",
+    text_node_properties=[
+        "physician_name",
+        "patient_name",
+        "text",
+        "hospital_name",
+    ],
+    embedding_node_property="embedding",
+)
