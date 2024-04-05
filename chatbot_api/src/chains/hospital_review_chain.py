8d531930da26a020f61e30760f707c3cb43ea2a4
Anergcorp <ahunatajanov@gmail.com>
2024-04-05 03:52:27 +0300

    creating RetrievalQA chain

--- chatbot_api/src/chains/hospital_review_chain.py
+++ chatbot_api/src/chains/hospital_review_chain.py
@@ -27,3 +27,31 @@ neo4j_vector_index = Neo4jVector.from_existing_graph(
     ],
     embedding_node_property="embedding",
 )
+
+review_template = """Your job is to use patient
+reviews to answer questions about their experience at a hospital. Use
+the following context to answer questions. Be as detailed as possible, but
+don't make up any information that's not from the context. If you don't know
+an answer, say you don't know.
+{context}
+"""
+
+review_system_prompt = SystemMessagePromptTemplate(
+    prompt=PromptTemplate(input_variables=["context"], template=review_template)
+)
+
+review_human_prompt = HumanMessagePromptTemplate(
+    prompt=PromptTemplate(input_variables=["question"], template="{question}")
+)
+messages = [review_system_prompt, review_human_prompt]
+
+review_prompt = ChatPromptTemplate(
+    input_variables=["context", "question"], messages=messages
+)
+
+reviews_vector_chain = RetrievalQA.from_chain_type(
+    llm=ChatOpenAI(model=HOSPITAL_QA_MODEL, temperature=0),
+    chain_type="stuff",
+    retriever=neo4j_vector_index.as_retriever(k=12),
+)
+reviews_vector_chain.combine_documents_chain.llm_chain.prompt = review_prompt
