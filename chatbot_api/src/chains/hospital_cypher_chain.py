2caf8bf0f2802eaea277c60f8301e9e48cf16896
Anergcorp <ahunatajanov@gmail.com>
2024-04-07 00:40:34 +0300

    creating GraphCypherChain

--- chatbot_api/src/chains/hospital_cypher_chain.py
+++ chatbot_api/src/chains/hospital_cypher_chain.py
@@ -104,3 +104,52 @@ The question is:
 cypher_generation_prompt = PromptTemplate(
     input_variables=["schema", "question"], template=cypher_generation_template
 )
+
+qa_generation_template = """You are an assistant that takes the results
+from a Neo4j Cypher query and forms a human-readable response. The
+query results section contains the results of a Cypher query that was
+generated based on a user's natural language question. The provided
+information is authoritative, you must never doubt it or try to use
+your internal knowledge to correct it. Make the answer sound like a
+response to the question.
+
+Query Results:
+{context}
+
+Question:
+{question}
+
+If the provided information is empty, say you don't know the answer.
+Empty information looks like this: []
+
+If the information is not empty, you must provide an answer using the
+results. If the question involves a time duration, assume the query
+results are in units of days unless otherwise specified.
+
+When names are provided in the query results, such as hospital names,
+beware  of any names that have commas or other punctuation in them.
+For instance, 'Jones, Brown and Murray' is a single hospital name,
+not multiple hospitals. Make sure you return any list of names in
+a way that isn't ambiguous and allows someone to tell what the full
+names are.
+
+Never say you don't have the right information if there is data in
+the query results. Always use the data in the query results.
+
+Helpful Answer:
+"""
+
+qa_generation_prompt = PromptTemplate(
+    input_variables=["context", "question"], template=qa_generation_template
+)
+
+ospital_cypher_chain = GraphCypherQAChain.from_llm(
+    cypher_llm=ChatOpenAI(model=HOSPITAL_CYPHER_MODEL, temperature=0),
+    qa_llm=ChatOpenAI(model=HOSPITAL_QA_MODEL, temperature=0),
+    graph=graph,
+    verbose=True,
+    qa_prompt=qa_generation_prompt,
+    cypher_prompt=cypher_generation_prompt,
+    validate_cypher=True,
+    top_k=100,
+)
