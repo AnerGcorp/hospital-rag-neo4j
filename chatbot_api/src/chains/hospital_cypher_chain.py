b00a0d8273d2dc16f3c48504ddb6febc961c4fcb
Anergcorp <ahunatajanov@gmail.com>
2024-04-06 23:55:03 +0300

    creating cypher chain

---
+++ chatbot_api/src/chains/hospital_cypher_chain.py
@@ -0,0 +1,16 @@
+import os
+from langchain_community.graphs import Neo4jGraph
+from langchain.chains import GraphCypherQAChain
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+
+HOSPITAL_QA_MODEL = os.getenv("HOSPITAL_QA_MODEL")
+HOSPITAL_CYPHER_MODEL = os.getenv("HOSPITAL_CYPHER_MODEL")
+
+graph = Neo4jGraph(
+    url=os.getenv("NEO4J_URI"),
+    username=os.getenv("NEO4J_USERNAME"),
+    password=os.getenv("NEO4J_PASSWORD"),
+)
+
+graph.refresh_schema()
