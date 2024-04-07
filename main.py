839fdc9502d14d8d56776322b57c9ca1c19d958f
Anergcorp <ahunatajanov@gmail.com>
2024-04-07 05:45:31 +0300

    adding frontend configuration to docker-compose

--- main.py
+++
@@ -1,4 +0,0 @@
-from langchain_intro.chatbot import hospital_agent_executor
-
-hospital_agent_executor.invoke({"input": "What is the current wait time at hospital C?"})
-hospital_agent_executor.invoke({"input": "What have patients said about their comfort at the hospital?"})
